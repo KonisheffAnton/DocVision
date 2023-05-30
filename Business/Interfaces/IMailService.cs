fb1ad08d499a4cc7eaabb033eb8e153ea11badb4
Anton <konysheffanton@yandex.ru>
2023-05-30 14:50:56 +0300

    full

--- Business/Interfaces/IMailService.cs
+++ Business/Interfaces/IMailService.cs
@@ -11,18 +11,6 @@ namespace DocVision.Business.Interfaces
 
         public Task<IEnumerable<MailBusinessModel>> GetAllMailsAsync();
 
-        public Task UpdateMailAsync(MailBusinessModel mailBusinessModel);
-
-        //public Task<OpenApplicationBusinessModel> GetOpenApplicationByDepositProductNameAndClientIdAsync(Guid clientId, string depositProductName);
-
-        //public Task UpdateOpenApplicationAsync(OpenApplicationBusinessModel openApplication);
-
-        //public Task<OpenApplicationCodeResult> TurnApplicationIsActiveFieldToFalseAsync(Guid clientId, string depositProductName);
-
-        //public Task<OpenApplicationCodeResult> SendOpenApplicationsAsync(Guid clientId, OpenApplicationBusinessModel depositApplication);
-
-
-
-        //public Task<bool> GetCheckDepositApplicationAsync(Guid clientId, string name);        
+        public Task UpdateMailAsync(MailBusinessModel mailBusinessModel);    
     }
 }
